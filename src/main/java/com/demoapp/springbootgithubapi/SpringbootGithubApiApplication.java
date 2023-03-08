f382815ee02771d04f144027872ac0e7f2fd9bc7
Andy <andrew.zerko@gmail.com>
2023-03-08 16:58:06 +0200

    Initial commit

---
+++ src/main/java/com/demoapp/springbootgithubapi/SpringbootGithubApiApplication.java
@@ -0,0 +1,12 @@
+package com.demoapp.springbootgithubapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGithubApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootGithubApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demoapp/springbootgithubapi/SpringbootGithubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoapp.springbootgithubapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGithubApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
