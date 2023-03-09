320c01ffd348fa59a30bbe1cbea163b3bc7ccabd
Andy <andrew.zerko@gmail.com>
2023-03-09 20:44:54 +0200

    Add RepositoryService and logic to get repositories without branches

--- src/main/java/com/demoapp/springbootgithubapi/service/github/RepositoryServiceImpl.java
+++ src/main/java/com/demoapp/springbootgithubapi/service/github/RepositoryServiceImpl.java
@@ -1,16 +1,30 @@
 package com.demoapp.springbootgithubapi.service.github;
 
+import com.demoapp.springbootgithubapi.client.GithubRestTemplate;
+import com.demoapp.springbootgithubapi.mapper.RepositoryMapper;
+import com.demoapp.springbootgithubapi.model.Repository;
 import com.demoapp.springbootgithubapi.payload.RepositoryDTO;
 import com.demoapp.springbootgithubapi.service.RepositoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
 import java.util.List;
 
 @Service
 public class RepositoryServiceImpl implements RepositoryService {
+    private final GithubRestTemplate githubRestTemplate;
+    private final RepositoryMapper repositoryMapper;
+
+    @Autowired
+    public RepositoryServiceImpl(GithubRestTemplate githubRestTemplate, RepositoryMapper repositoryMapper) {
+        this.githubRestTemplate = githubRestTemplate;
+        this.repositoryMapper = repositoryMapper;
+    }
+
     @Override
     public List<RepositoryDTO> getAllNonForkedRepositoriesByUsername(String username) {
-        return Collections.emptyList();
+        List<Repository> repositories = githubRestTemplate.getUserRepositoriesByUsername(username);
+
+        return repositories.stream().map(repositoryMapper::repositoryToRepositoryDto).toList();
     }
 }
--- src/test/java/com/demoapp/springbootgithubapi/service/github/RepositoryServiceImplTest.java
+++ src/test/java/com/demoapp/springbootgithubapi/service/github/RepositoryServiceImplTest.java
@@ -1,18 +1,66 @@
 package com.demoapp.springbootgithubapi.service.github;
 
-import org.junit.jupiter.api.BeforeEach;
+import com.demoapp.springbootgithubapi.client.GithubRestTemplate;
+import com.demoapp.springbootgithubapi.mapper.RepositoryMapper;
+import com.demoapp.springbootgithubapi.model.Owner;
+import com.demoapp.springbootgithubapi.model.Repository;
+import com.demoapp.springbootgithubapi.payload.RepositoryDTO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class RepositoryServiceImplTest {
-    @BeforeEach
-    void setUp() {
+    private static GithubRestTemplate githubRestTemplateMock;
+    private static RepositoryServiceImpl repositoryService;
 
+    @BeforeAll
+    static void beforeAll() {
+        githubRestTemplateMock = mock(GithubRestTemplate.class);
+        repositoryService = new RepositoryServiceImpl(githubRestTemplateMock, Mappers.getMapper(RepositoryMapper.class));
     }
 
     @Test
-    void getAllNonForkedRepositoriesByUsername() {
-        fail("Not yet implemented");
+    void getAllNonForkedRepositoriesByUsernameShouldWorkCorrectWithZeroRepositories() {
+        when(githubRestTemplateMock.getUserRepositoriesByUsername(anyString()))
+                .thenReturn(Collections.emptyList());
+
+        List<RepositoryDTO> repositories = repositoryService.getAllNonForkedRepositoriesByUsername(anyString());
+        Assertions.assertEquals(0, repositories.size());
+    }
+
+    @Test
+    void getAllNonForkedRepositoriesByUsernameShouldWorkCorrectWithNoneZeroRepositories() {
+        when(githubRestTemplateMock.getUserRepositoriesByUsername(anyString()))
+                .thenReturn(Arrays.asList(new Repository(), new Repository(), new Repository()));
+
+        List<RepositoryDTO> repositories = repositoryService.getAllNonForkedRepositoriesByUsername(anyString());
+        Assertions.assertEquals(3, repositories.size());
+    }
+
+    @Test
+    void getAllNonForkedRepositoriesByUsernameMappedCorrectly() {
+        Owner owner = new Owner();
+        owner.setLogin("test owner");
+
+        Repository repository = new Repository();
+        repository.setName("test repository");
+        repository.setOwner(owner);
+
+        when(githubRestTemplateMock.getUserRepositoriesByUsername(anyString()))
+                .thenReturn(List.of(repository));
+
+        List<RepositoryDTO> repositoriesDTOs = repositoryService.getAllNonForkedRepositoriesByUsername(anyString());
+
+        Assertions.assertEquals(repositoriesDTOs.get(0).getName(), repository.getName());
+        Assertions.assertEquals(repositoriesDTOs.get(0).getOwnerLogin(), repository.getOwner().getLogin());
     }
 }
