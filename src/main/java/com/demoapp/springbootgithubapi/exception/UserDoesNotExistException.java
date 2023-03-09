d3864baaadfc64101714b4798b0d049efadf92eb
Andy <andrew.zerko@gmail.com>
2023-03-09 23:22:56 +0200

    Add getter to UserDoesNotExistException

--- src/main/java/com/demoapp/springbootgithubapi/exception/UserDoesNotExistException.java
+++ src/main/java/com/demoapp/springbootgithubapi/exception/UserDoesNotExistException.java
@@ -1,11 +1,16 @@
 package com.demoapp.springbootgithubapi.exception;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@Getter
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class UserDoesNotExistException extends RuntimeException {
+    private final String username;
+
     public UserDoesNotExistException(String username) {
         super(String.format("There is no user with username: %s", username));
+        this.username = username;
     }
 }
