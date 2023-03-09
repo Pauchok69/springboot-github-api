c550636f6bed864daafa6daf54c9d9de8e78aadf
Andy <andrew.zerko@gmail.com>
2023-03-09 18:28:50 +0200

    Add Mapper for Repository and RepositoryDTO

---
+++ src/main/java/com/demoapp/springbootgithubapi/mapper/RepositoryMapper.java
@@ -0,0 +1,12 @@
+package com.demoapp.springbootgithubapi.mapper;
+
+import com.demoapp.springbootgithubapi.model.Repository;
+import com.demoapp.springbootgithubapi.payload.RepositoryDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface RepositoryMapper {
+    @Mapping(target = "ownerLogin", source = "entity.owner.login")
+    RepositoryDTO repositoryToRepositoryDto(Repository entity);
+}
