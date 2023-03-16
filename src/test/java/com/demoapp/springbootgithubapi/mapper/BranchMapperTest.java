560d840d1d4d19b68cbe84fa7d6943b0bca5e498
andy <pauchokbk@gmail.com>
2023-03-16 17:18:11 +0200

    Add tests for Mappers

---
+++ src/test/java/com/demoapp/springbootgithubapi/mapper/BranchMapperTest.java
@@ -0,0 +1,37 @@
+package com.demoapp.springbootgithubapi.mapper;
+
+import com.demoapp.springbootgithubapi.model.Branch;
+import com.demoapp.springbootgithubapi.model.Commit;
+import com.demoapp.springbootgithubapi.payload.BranchDTO;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mapstruct.factory.Mappers;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class BranchMapperTest {
+    @Spy
+    private BranchMapper branchMapper = Mappers.getMapper(BranchMapper.class);
+
+    @Test
+    void branchToBranchDtoShouldWorkAsExpected() {
+        Commit commit = new Commit();
+        commit.setSha("Test Sha");
+
+        Branch branch = new Branch();
+        branch.setName("Test name");
+        branch.setCommit(commit);
+
+        BranchDTO branchDTO = branchMapper.branchToBranchDto(branch);
+
+        assertEquals(branchDTO.getName(), branch.getName());
+        assertEquals(branchDTO.getLastCommitSha(), branch.getCommit().getSha());
+        verify(branchMapper, times(1)).branchToBranchDto(any(Branch.class));
+    }
+}
---
+++ src/test/java/com/demoapp/springbootgithubapi/mapper/RepositoryMapperTest.java
@@ -0,0 +1,41 @@
+package com.demoapp.springbootgithubapi.mapper;
+
+import com.demoapp.springbootgithubapi.model.Owner;
+import com.demoapp.springbootgithubapi.model.Repository;
+import com.demoapp.springbootgithubapi.payload.RepositoryDTO;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mapstruct.factory.Mappers;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class RepositoryMapperTest {
+    @Spy
+    private RepositoryMapper repositoryMapper = Mappers.getMapper(RepositoryMapper.class);
+
+    @Test
+    void repositoryToRepositoryDtoShouldWorkAsExpected() {
+        Owner owner = new Owner();
+        owner.setLogin("Test username");
+
+        Repository repository = new Repository();
+        repository.setFork(false);
+        repository.setName("Test Name");
+        repository.setOwner(owner);
+
+        RepositoryDTO repositoryDTO = repositoryMapper.repositoryToRepositoryDto(repository);
+
+        assertEquals(repositoryDTO.getName(), repository.getName());
+        assertEquals(repositoryDTO.getOwnerLogin(), repository.getOwner().getLogin());
+        assertNull(repositoryDTO.getBranches());
+
+        verify(repositoryMapper, times(1)).repositoryToRepositoryDto(any(Repository.class));
+    }
+}
